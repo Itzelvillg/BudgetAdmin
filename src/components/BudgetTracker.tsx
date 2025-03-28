4a2c1413b46cc9332c46364b57aa0f484feec664
Itzel Villasenor <itzelvillg@gmail.com>
2025-03-27 19:33:16 -0600

    a

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -11,7 +11,7 @@ export const BudgetTracker = () => {
     <>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="flex justify-center">
-          <img src="https://img.icons8.com/office/452/money.png" alt="money" className="w-40 h-40" />
+          <CircularProgressbar value={state.budget} />
         </div>
         <div className="flex flex-col justify-end items-center gap-8">
           <button className="bg-pink-600 w-full p-2 uppercase rounded-lg text-white font-bold" onClick={() => dispatch({ type: "RESET_APP" })}>Reset APP</button>
