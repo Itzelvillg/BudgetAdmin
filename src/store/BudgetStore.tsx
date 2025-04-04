5b8db71a7fc859841ee59c646000c85fcb9e1731
itzel Villasenor <itzelvillg@gmail.com>
2025-04-03 23:05:05 -0600

    v1 zustand store

--- src/store/BudgetStore.tsx
+++ src/store/BudgetStore.tsx
@@ -1,21 +1,43 @@
 import { create } from 'zustand';
-import { Expense } from '../types';
+import { Category, Expense } from '../types';
 
 
 type BudgetStoreType = {
   budget: number;
   expenses: Expense[];
-  setExpenses: (expenses: Expense[]) => void;
   savings: number;
+  currentCategory: Category['id'];
+  isModalOpen: boolean;
+  editingExpense: Expense['id'];
+
+
   setBudget: (initialBudget: number) => void;
   resetBudget: () => void;
-};
+  addExpense: (expenses: Expense[]) => void;
+  setCurrentCategory: (category: Category['id']) => void;
+  setIsModalOpen: (isOpen: boolean) => void;
+  setEditingExpense: (expenseId: Expense['id']) => void;
+  setSavings: (savings: number) => void;
+  deleteExpense: (expenseId: Expense['id']) => void;
+  
 
 export const useBudgetStore = create<BudgetStoreType>((set) => ({
-  budget: 0,
-  savings: 0,
-  expenses: [],
-  setBudget: (initialBudget) => set(() => ({ budget: initialBudget })),
-  setExpenses: (expenses) => set(() => ({ expenses })),
-  resetBudget: () => set({ budget: 0 }),
-}));
+    budget: 0,
+    savings: 0,
+    expenses: [],
+    currentCategory: '',
+    isModalOpen: false,
+    editingExpense: '',
+
+    setBudget: (initialBudget) => set(() => ({ budget: initialBudget })),
+    resetBudget: () => set({ budget: 0 }),
+    addExpense: (expenses) => set(() => ({ expenses })),
+    setCurrentCategory: (category) => set(() => ({ currentCategory: category })),
+    setIsModalOpen: (isOpen) => set(() => ({ isModalOpen: isOpen })),
+    setEditingExpense: (expenseId) => set(() => ({ editingExpense: expenseId })),
+    setSavings: (savings) => set(() => ({ savings })),
+    deleteExpense: (expenseId) =>
+      set((state) => ({
+        expenses: state.expenses.filter((expense) => expense.id !== expenseId),
+      })),
+  }));
